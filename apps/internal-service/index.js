51789efdf79e0fd11bb7f050e96806dfbef1b7c2
Saurav Sahu <mrsauravsahu@outlook.com>
2022-07-19 16:39:09 +0530

    improv: add message to root route

--- apps/internal-service/index.js
+++ apps/internal-service/index.js
@@ -5,7 +5,7 @@ const app = path.basename(__dirname)
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write(JSON.stringify({ app }));
+  res.write(JSON.stringify({ app, message: null }));
   res.end();
 })
 
