919a14c704f3a2d7ada7b008f0d8dc34cdf3fd66
Saurav Sahu <mrsauravsahu@outlook.com>
2022-08-02 18:18:20 +0530

    feat: remove message property (#19)
    * feat: remove message property
    
    * fix: remove next-version
    
    * remove next-version from gitversion

--- apps/internal-service/index.js
+++ apps/internal-service/index.js
@@ -7,7 +7,6 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.write(JSON.stringify({
     app,
-    message: `Message for '${app}'`,
     version: '1.0.0'
   }));
   res.end();
